7c03ba6573c9499f13babf718a9222f196c6bdab
tanvir-hadan-code <tanvirx015@gmail.com>
2025-11-04 17:33:20 +0600

    fix(Image): nextConfig image remotePatterns Add

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   /* config options here */
   images: {
-    domains: ['i.ibb.co'], 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+    ],
   },
 };
 
